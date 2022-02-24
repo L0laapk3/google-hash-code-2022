fb05308b8b766f96acb0070a1fb01b792d6b2510
L0laapk3 <L0laapk3@gmail.com>
2022-02-24 21:35:36 +0100

    optimise score

--- rand.py
+++ rand.py
@@ -54,155 +54,166 @@ def main(infile):
 			return str(self.name) + ':' + str(self.duration) + ':' + str(self.maxScore) + ':' + str(self.bestBefore) + ':' + str(self.skillsReq) 
 
 
+	maxScore = 0
 
-	with open("in/" + infile, 'r') as inf:
+	while True:
+		with open("in/" + infile, 'r') as inf:
 
-		infoLine = inf.readline().rstrip("\n")
-		info = infoLine.split(" ")
+			infoLine = inf.readline().rstrip("\n")
+			info = infoLine.split(" ")
 
-		numContrib = int(info[0])
-		numProj = int(info[1])
+			numContrib = int(info[0])
+			numProj = int(info[1])
 
-		allContrib = []
-		for contribI in range(numContrib):
-			contribLine = inf.readline().rstrip("\n")
-			contribInfo = contribLine.split(" ")
-			name = contribInfo[0]
-			numSkills = int(contribInfo[1])
-			skills = []
-			for skillI in range(numSkills):
-				skillLine = inf.readline().rstrip("\n")
-				skillInfo = skillLine.split(" ")
-				skills.append(Skill(skillInfo[0], int(skillInfo[1])))
+			allContrib = []
+			allContribBySkill = {}
+			for contribI in range(numContrib):
+				contribLine = inf.readline().rstrip("\n")
+				contribInfo = contribLine.split(" ")
+				name = contribInfo[0]
+				numSkills = int(contribInfo[1])
+				skills = []
+				contrib = Contrib(contribInfo[0], skills)
+				for skillI in range(numSkills):
+					skillLine = inf.readline().rstrip("\n")
+					skillInfo = skillLine.split(" ")
+					skills.append(Skill(skillInfo[0], int(skillInfo[1])))
+					if skillInfo[0] not in allContribBySkill:
+						allContribBySkill[skillInfo[0]] = [contrib]
+					else:
+						allContribBySkill[skillInfo[0]].append(contrib)
 
-			contrib = Contrib(contribInfo[0], skills)
-			allContrib.append(contrib)
+				allContrib.append(contrib)
 
-		lastDay = 0
-		allProj = []
-		for projI in range(numProj):
-			projLine = inf.readline().rstrip("\n")
-			projInfo = projLine.split(" ")
-			name = projInfo[0]
-			duration = int(projInfo[1])
-			score = int(projInfo[2])
-			bestBefore = int(projInfo[3])
-			numRoles = int(projInfo[4])
-			roles = []
-			for roleI in range(numRoles):
-				skillLine = inf.readline().rstrip("\n")
-				skillInfo = skillLine.split(" ")
-				roles.append(Skill(skillInfo[0], int(skillInfo[1])))
+			lastDay = 0
+			allProj = []
+			for projI in range(numProj):
+				projLine = inf.readline().rstrip("\n")
+				projInfo = projLine.split(" ")
+				name = projInfo[0]
+				duration = int(projInfo[1])
+				score = int(projInfo[2])
+				bestBefore = int(projInfo[3])
+				numRoles = int(projInfo[4])
+				roles = []
+				for roleI in range(numRoles):
+					skillLine = inf.readline().rstrip("\n")
+					skillInfo = skillLine.split(" ")
+					roles.append(Skill(skillInfo[0], int(skillInfo[1])))
 
-			lastDay = max(lastDay, bestBefore + duration)			
-			proj = Proj(name, duration, score, bestBefore, tuple(roles))
-			allProj.append(proj)
+				lastDay = max(lastDay, bestBefore + duration)			
+				proj = Proj(name, duration, score, bestBefore, tuple(roles))
+				allProj.append(proj)
 
-	# print(allContrib)
-	# print(allProj)
+		# print(allContrib)
+		# print(allProj)
 
 
-	## random
-	def fillDay(day):
-		random.shuffle(allProj)
-		for proj in allProj:
-			if proj.startDay != -1: # project booked
-				break
-			random.shuffle(allContrib)
-			unfilledSkills = list(proj.skillsReq)
-			unfilledCount = len(unfilledSkills)
-			# print(unfilledCount)
-			for contrib in allContrib:
-				# todo: check if person is busy
-				isBusy = False
-				for busy in contrib.busy:
-					if busy[0] <= day and day < busy[1]:
-						isBusy = True
-						break
-				if isBusy:
+		## random
+		def fillDay(day):
+			random.shuffle(allProj)
+			for proj in allProj:
+				if proj.startDay != -1: # project booked
 					break
-				usedContrib = False
-				for skillReqI in range(len(unfilledSkills)):
-					skillReq = unfilledSkills[skillReqI]
-					if skillReq is not None:
-						for skill in contrib.skills:
-							if skillReq.name == skill.name and skillReq.level <= skill.level:
-								# print(skillReq)
-								proj.contribs[skillReqI] = contrib
-								unfilledSkills[skillReqI] = None
-								usedContrib = True
-								unfilledCount -= 1
-								if unfilledCount == 0:
-									proj.startDay = day
-								break
-					if usedContrib:
+				random.shuffle(allContrib)
+				unfilledSkills = list(proj.skillsReq)
+				unfilledCount = len(unfilledSkills)
+				# print(unfilledCount)
+				for contrib in allContrib:
+					# todo: check if person is busy
+					isBusy = False
+					for busy in contrib.busy:
+						if busy[0] <= day and day < busy[1]:
+							isBusy = True
+							break
+					if isBusy:
 						break
-				if proj.startDay != -1: # project booked now
-					for contribI in range(len(proj.contribs)):
-						contrib = proj.contribs[contribI]
-						contrib.busy.append((day, day + proj.duration))
-						for skill in contrib.skills:
-							if skill.name == proj.skillsReq[contribI].name and skill.level <= proj.skillsReq[contribI].level:
-								skill.level += 1
-					break
-				
+					usedContrib = False
+					for skillReqI in range(len(unfilledSkills)):
+						skillReq = unfilledSkills[skillReqI]
+						if skillReq is not None:
+							for skill in contrib.skills:
+								if skillReq.name == skill.name and skillReq.level <= skill.level:
+									# print(skillReq)
+									proj.contribs[skillReqI] = contrib
+									unfilledSkills[skillReqI] = None
+									usedContrib = True
+									unfilledCount -= 1
+									if unfilledCount == 0:
+										proj.startDay = day
+									break
+						if usedContrib:
+							break
+					if proj.startDay != -1: # project booked now
+						for contribI in range(len(proj.contribs)):
+							contrib = proj.contribs[contribI]
+							contrib.busy.append((day, day + proj.duration))
+							for skill in contrib.skills:
+								if skill.name == proj.skillsReq[contribI].name and skill.level <= proj.skillsReq[contribI].level:
+									skill.level += 1
+						break
+					
 
 
 
-	for day in range(lastDay):
-		fillDay(day)
+		for day in range(lastDay):
+			fillDay(day)
 
 
 
 
-	def getTotalScore():
-		score = 0
-		for proj in allProj:
-			if proj.startDay != -1:
-				score += max(proj.maxScore - max(proj.startDay + proj.duration - proj.bestBefore, 0), 0)
-		return score
+		def getTotalScore():
+			score = 0
+			for proj in allProj:
+				if proj.startDay != -1:
+					score += max(proj.maxScore - max(proj.startDay + proj.duration - proj.bestBefore, 0), 0)
+			return score
 
-	print(getTotalScore())
-	
+		score = getTotalScore()
+		if score <= maxScore:
+			continue
+		print(score)
+		maxScore = score
+		
 
-	# allProj[1].startDay = 0
-	# allProj[1].contribs[1] = allContrib[0]
-	# allProj[1].contribs[0] = allContrib[1]
+		# allProj[1].startDay = 0
+		# allProj[1].contribs[1] = allContrib[0]
+		# allProj[1].contribs[0] = allContrib[1]
 
-	# allProj[0].startDay = 7
-	# allProj[0].contribs[0] = allContrib[0]
+		# allProj[0].startDay = 7
+		# allProj[0].contribs[0] = allContrib[0]
 
-	# allProj[2].startDay = 7
-	# allProj[2].contribs[0] = allContrib[2]
-	# allProj[2].contribs[1] = allContrib[1]
+		# allProj[2].startDay = 7
+		# allProj[2].contribs[0] = allContrib[2]
+		# allProj[2].contribs[1] = allContrib[1]
 
 
-	# print(allContrib)
+		# print(allContrib)
 
-	
-	with open("out" + infile, "w") as txt_file:
-		plannedProj = []
-		for proj in allProj:
-			if proj.startDay != -1:
-				plannedProj.append(proj)
-				
-		outProj = list(filter(lambda p: p.startDay != -1, allProj))
-		outProj = sorted(outProj, key=lambda p: p.startDay)
-		txt_file.write(f"{str(len(outProj))}\n")
-		for proj in outProj:
-			txt_file.write(f"{proj.name}\n")
-			contribNames = []
-			for contrib in proj.contribs:
-				contribNames.append(contrib.name)
-			txt_file.write(f"{' '.join(contribNames)}\n")
+		
+		with open("out" + infile, "w") as txt_file:
+			plannedProj = []
+			for proj in allProj:
+				if proj.startDay != -1:
+					plannedProj.append(proj)
+					
+			outProj = list(filter(lambda p: p.startDay != -1, allProj))
+			outProj = sorted(outProj, key=lambda p: p.startDay)
+			txt_file.write(f"{str(len(outProj))}\n")
+			for proj in outProj:
+				txt_file.write(f"{proj.name}\n")
+				contribNames = []
+				for contrib in proj.contribs:
+					contribNames.append(contrib.name)
+				txt_file.write(f"{' '.join(contribNames)}\n")
 
-	print(f"done {infile}")
+		print(f"done {infile}")
 
 
 
 
 if __name__ == "__main__":
-	main("a.txt")
+	# main("a.txt")
 	main("b.txt")
 	# main("c.txt")
 	# main("d.txt")
